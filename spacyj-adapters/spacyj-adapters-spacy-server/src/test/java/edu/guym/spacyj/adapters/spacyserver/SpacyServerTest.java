06fabe29c56e69229db7a64e19671a35c83f6357
manzurola <guy.manzurola@gmail.com>
2021-06-19 14:20:24 +0300

    spacy server container test

---
+++ spacyj-adapters/spacyj-adapters-spacy-server/src/test/java/edu/guym/spacyj/adapters/spacyserver/SpacyServerTest.java
@@ -0,0 +1,45 @@
+package edu.guym.spacyj.adapters.spacyserver;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.guym.spacyj.api.Spacy;
+import edu.guym.spacyj.api.containers.Doc;
+import edu.guym.spacyj.api.json.SpacyJacksonModule;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Testcontainers
+public class SpacyServerTest {
+
+    private Spacy spacy;
+
+    @Container
+    public final GenericContainer<?> spacyServer = new GenericContainer<>("neelkamath/spacy-server:2-en_core_web_sm")
+            .withExposedPorts(8000);
+
+    @BeforeEach
+    void setup() {
+        String host = spacyServer.getHost();
+        Integer port = spacyServer.getFirstMappedPort();
+        spacy = Spacy.create(SpacyServerAdapter.create(host, port));
+    }
+
+    @Test
+    public void verifySpacyServerResponse() throws IOException {
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new SpacyJacksonModule());
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("spacy-server-response.json");
+        Doc expected = mapper.readValue(inputStream, Doc.class);
+
+        Doc actual = spacy.nlp("Hello world. How are you today?");
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
