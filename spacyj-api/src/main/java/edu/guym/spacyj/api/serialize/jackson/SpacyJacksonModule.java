e1d8a2f0722aa9a9c49deb6f38680d2ff3743054
manzurola <guy.manzurola@gmail.com>
2021-06-25 16:42:00 +0300

    ignore unknown props in jackson

--- spacyj-api/src/main/java/edu/guym/spacyj/api/serialize/jackson/SpacyJacksonModule.java
+++ spacyj-api/src/main/java/edu/guym/spacyj/api/serialize/jackson/SpacyJacksonModule.java
@@ -1,6 +1,7 @@
 package edu.guym.spacyj.api.serialize.jackson;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import edu.guym.spacyj.api.containers.Doc;
@@ -13,7 +14,8 @@ public class SpacyJacksonModule extends SimpleModule {
         ObjectMapper mapper = new ObjectMapper()
                 .setVisibility(ALL, JsonAutoDetect.Visibility.NONE)
                 .setVisibility(CREATOR, JsonAutoDetect.Visibility.ANY)
-                .setVisibility(FIELD, JsonAutoDetect.Visibility.ANY);
+                .setVisibility(FIELD, JsonAutoDetect.Visibility.ANY)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         addSerializer(Doc.class, new DocSerializer(mapper));
         addDeserializer(Doc.class, new DocDeserializer(mapper));
     }
