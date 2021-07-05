e29dad51d649c18465da65f4ce602fad10931f9d
manzurola <guy.manzurola@gmail.com>
2021-07-05 21:16:57 +0300

    spacy as interface

--- spacy4j-api/src/main/java/io/languagetoys/spacy4j/api/SpaCy.java
+++ spacy4j-api/src/main/java/io/languagetoys/spacy4j/api/SpaCy.java
@@ -1,35 +1,13 @@
 package io.languagetoys.spacy4j.api;
 
 import io.languagetoys.spacy4j.api.containers.Doc;
-import io.languagetoys.spacy4j.api.containers.TokenData;
 import io.languagetoys.spacy4j.api.exceptions.SpaCyException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.util.List;
-import java.util.Objects;
+public interface SpaCy {
 
-public final class SpaCy {
+    Doc nlp(String text) throws SpaCyException;
 
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
-    private final SpaCyAdapter adapter;
-
-    private SpaCy(SpaCyAdapter adapter) {
-        this.adapter = adapter;
-    }
-
-    public static SpaCy create(SpaCyAdapter adapter) {
-        return new SpaCy(adapter);
-    }
-
-    public final Doc nlp(String text) throws SpaCyException {
-        Objects.requireNonNull(text);
-        try {
-            List<TokenData> data = adapter.nlp(text);
-            return Doc.create(text, data);
-        } catch (Throwable e) {
-            logger.error("failed to parse text", e);
-            throw new SpaCyException(e, text);
-        }
+    static SpaCy create(SpaCyAdapter adapter) {
+        return new SpaCyImpl(adapter);
     }
 }
